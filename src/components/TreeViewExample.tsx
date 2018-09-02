7f96b1ebb165946d2058598d5cbf96f0f2684441
baurine <2008.hbl@gmail.com>
2018-09-02 15:11:39 +0800

    Show selected status

--- src/components/TreeViewExample.tsx
+++ src/components/TreeViewExample.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import TreeView, { INode, IGroup } from './TreeView'
+import TreeView, { INode, IGroup, IItem } from './TreeView'
 import './TreeView.css'
 
 type State = {
   treeViewData: (INode | IGroup)[]
+  selectedTreeViewItem: IItem | null
 }
 
 const initialState: State = {
@@ -12,21 +13,27 @@ const initialState: State = {
     {key: '2', parentKey: '', kind: 'group', name: 'group 1', expand: false},
     {key: '3', parentKey: '', kind: 'node',  name: 'node 2'},
     {key: '4', parentKey: '', kind: 'group', name: 'group 2', expand: false}
-  ]
+  ],
+  selectedTreeViewItem: null
 }
 
 export default class TreeViewExample extends React.Component<object, State> {
   readonly state = initialState
 
+  onTreeViewItemSelected = (item: IItem | null) => {
+    this.setState({selectedTreeViewItem: item})
+  }
+
   render() {
-    const { treeViewData } = this.state
+    const { treeViewData, selectedTreeViewItem } = this.state
     return (
       <div className="tree-view-container">
         <TreeView
           layer={0}
           data={treeViewData}
           rootParentKey=''
-          selectedItem={null}/>
+          selectedItem={selectedTreeViewItem}
+          onItemSelected={this.onTreeViewItemSelected}/>
       </div>
     )
   }
