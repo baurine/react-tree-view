d7cfe24a211d6bd3fdb23096c8b22c6b354abbe8
baurine <2008.hbl@gmail.com>
2018-09-02 15:16:29 +0800

    Toggle group expand status

--- src/components/TreeViewExample.tsx
+++ src/components/TreeViewExample.tsx
@@ -12,7 +12,8 @@ const initialState: State = {
     {key: '1', parentKey: '', kind: 'node',  name: 'node 1'},
     {key: '2', parentKey: '', kind: 'group', name: 'group 1', expand: false},
     {key: '3', parentKey: '', kind: 'node',  name: 'node 2'},
-    {key: '4', parentKey: '', kind: 'group', name: 'group 2', expand: false}
+    {key: '4', parentKey: '', kind: 'group', name: 'group 2', expand: false},
+    {key: '4_1', parentKey: '4', kind: 'node', name: 'node 3'}
   ],
   selectedTreeViewItem: null
 }
@@ -24,6 +25,17 @@ export default class TreeViewExample extends React.Component<object, State> {
     this.setState({selectedTreeViewItem: item})
   }
 
+  onTreeViewItemExpandToggle = (item: IGroup) => {
+    const newItems =
+      this.state.treeViewData.map(data => {
+        if (data.key === item.key) {
+          return {...item, expand: !item.expand}
+        }
+        return data
+      })
+    this.setState({treeViewData: newItems})
+  }
+
   render() {
     const { treeViewData, selectedTreeViewItem } = this.state
     return (
@@ -33,7 +45,8 @@ export default class TreeViewExample extends React.Component<object, State> {
           data={treeViewData}
           rootParentKey=''
           selectedItem={selectedTreeViewItem}
-          onItemSelected={this.onTreeViewItemSelected}/>
+          onItemSelected={this.onTreeViewItemSelected}
+          onGroupExpandToggle={this.onTreeViewItemExpandToggle}/>
       </div>
     )
   }
